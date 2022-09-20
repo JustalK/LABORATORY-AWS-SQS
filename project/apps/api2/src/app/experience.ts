1ac2cc596958b9f483a228f7ee6d69a6e0ca05ae
justalk <justal.kevin@gmail.com>
2022-09-20 21:53:41 +0200

    [FEATURE] Improving SQS

--- project/apps/api2/src/app/experience.ts
+++ project/apps/api2/src/app/experience.ts
@@ -10,6 +10,35 @@ const SQS_CONFIG = {
 
 const sqs = new AWS.SQS(SQS_CONFIG);
 
+const queueURL = process.env.SQS_QUEUE_URL;
+
+var params = {
+  AttributeNames: ['SentTimestamp'],
+  MaxNumberOfMessages: 10,
+  MessageAttributeNames: ['All'],
+  QueueUrl: queueURL,
+  VisibilityTimeout: 20,
+  WaitTimeSeconds: 0,
+};
+
+sqs.receiveMessage(params, function (err, data) {
+  if (err) {
+    console.log('Receive Error', err);
+  } else if (data.Messages) {
+    var deleteParams = {
+      QueueUrl: queueURL,
+      ReceiptHandle: data.Messages[0].ReceiptHandle,
+    };
+    sqs.deleteMessage(deleteParams, function (err, data) {
+      if (err) {
+        console.log('Delete Error', err);
+      } else {
+        console.log('Message Deleted', data);
+      }
+    });
+  }
+});
+
 router.get('/health', (req, res) => {
   res.send({ status: 'working' });
 });
