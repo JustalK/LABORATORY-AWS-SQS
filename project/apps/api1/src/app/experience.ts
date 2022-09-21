67c9a80c05e12f4567b2a36d82ccd4879381e96a
justalk <justal.kevin@gmail.com>
2022-09-21 21:46:12 +0200

    [FEATURE] Playing with polling

--- project/apps/api1/src/app/experience.ts
+++ project/apps/api1/src/app/experience.ts
@@ -12,8 +12,6 @@ const sqs = new AWS.SQS(SQS_CONFIG);
 
 router.get('/send', (req, res) => {
   var params = {
-    // Remove DelaySeconds parameter and value for FIFO queues
-    DelaySeconds: 10,
     MessageAttributes: {
       Title: {
         DataType: 'String',
@@ -32,7 +30,7 @@ router.get('/send', (req, res) => {
       'Information about current NY Times fiction bestseller for week of 12/11/2016.',
     // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
     // MessageGroupId: "Group1",  // Required for FIFO queues
-    QueueUrl: process.env.SQS_QUEUE_URL,
+    QueueUrl: `${process.env.SQS_QUEUE_URL}#request`,
   };
 
   sqs.sendMessage(params, function (err, data) {
@@ -45,6 +43,34 @@ router.get('/send', (req, res) => {
 
   res.send(true);
 });
+
+sqs.receiveMessage(
+  {
+    AttributeNames: ['SentTimestamp'],
+    MaxNumberOfMessages: 10,
+    MessageAttributeNames: ['All'],
+    QueueUrl: `${process.env.SQS_QUEUE_URL}#reply`,
+    VisibilityTimeout: 20,
+    WaitTimeSeconds: 0,
+  },
+  function (err, data) {
+    if (err) {
+      console.log('Receive Error', err);
+    } else if (data.Messages) {
+      var deleteParams = {
+        QueueUrl: `${process.env.SQS_QUEUE_URL}#reply`,
+        ReceiptHandle: data.Messages[0].ReceiptHandle,
+      };
+      sqs.deleteMessage(deleteParams, function (err, data) {
+        if (err) {
+          console.log('Delete Error', err);
+        } else {
+          console.log('Message Deleted', data);
+        }
+      });
+    }
+  }
+);
 
 router.get('/health', (req, res) => {
   res.send({ status: 'working' });
--- project/apps/api2/src/app/experience.ts
+++ project/apps/api2/src/app/experience.ts
@@ -12,21 +12,23 @@ const sqs = new AWS.SQS(SQS_CONFIG);
 
 const queueURL = process.env.SQS_QUEUE_URL;
 
+/**
 var params = {
   AttributeNames: ['SentTimestamp'],
   MaxNumberOfMessages: 10,
   MessageAttributeNames: ['All'],
-  QueueUrl: queueURL,
+  QueueUrl: `${process.env.SQS_QUEUE_URL}#request`,
   VisibilityTimeout: 20,
   WaitTimeSeconds: 0,
 };
+
 
 sqs.receiveMessage(params, function (err, data) {
   if (err) {
     console.log('Receive Error', err);
   } else if (data.Messages) {
     var deleteParams = {
-      QueueUrl: queueURL,
+      QueueUrl: `${process.env.SQS_QUEUE_URL}#request`,
       ReceiptHandle: data.Messages[0].ReceiptHandle,
     };
     sqs.deleteMessage(deleteParams, function (err, data) {
@@ -36,8 +38,73 @@ sqs.receiveMessage(params, function (err, data) {
         console.log('Message Deleted', data);
       }
     });
+
+    sqs.sendMessage(
+      {
+        DelaySeconds: 10,
+        MessageAttributes: {
+          Test: {
+            DataType: 'String',
+            StringValue: 'Data1',
+          },
+        },
+        MessageBody: 'This is a test',
+        QueueUrl: `${process.env.SQS_QUEUE_URL}#reply`,
+      },
+      function (err, data) {
+        if (err) {
+          console.log('Error', err);
+        } else {
+          console.log('Success', data.MessageId);
+        }
+      }
+    );
   }
 });
+**/
+
+var receiveMessageParams = {
+  QueueUrl: `${process.env.SQS_QUEUE_URL}#request`,
+  MaxNumberOfMessages: 10,
+  VisibilityTimeout: 10,
+  WaitTimeSeconds: 10,
+};
+
+var receiveMessage = function () {
+  console.log('RUN');
+  sqs.receiveMessage(receiveMessageParams, function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+    if (data.Messages) {
+      for (var i = 0; i < data.Messages.length; i++) {
+        var message = data.Messages[i];
+        console.log(data.Messages);
+        removeFromQueue(message);
+      }
+      receiveMessage();
+    } else {
+      console.log('TIMEDOUT');
+      setTimeout(function () {
+        receiveMessage();
+      }, 10 * 1000);
+    }
+  });
+};
+
+var removeFromQueue = function (message) {
+  sqs.deleteMessage(
+    {
+      QueueUrl: `${process.env.SQS_QUEUE_URL}#request`,
+      ReceiptHandle: message.ReceiptHandle,
+    },
+    function (err, data) {
+      err && console.log(err);
+    }
+  );
+};
+
+receiveMessage();
 
 router.get('/health', (req, res) => {
   res.send({ status: 'working' });
